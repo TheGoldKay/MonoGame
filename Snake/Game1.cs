68b7e93cdb46bcae12348f958d52ebd6ce812a18
TheGoldKay <jhonnatanwfc@live.com>
2024-02-29 19:32:43 -0300

    snake's part (body element)

--- Snake/Game1.cs
+++ Snake/Game1.cs
@@ -10,10 +10,13 @@ namespace Snake
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         string fontName = "gamefont";
+        string partName = "snake_part";
         SpriteFont font;
         Vector2 titlePos;
         int window_width = 800;
         int window_height = 600;
+        Texture2D snakePart;
+        Vector2 headPos;
 
         public Game1()
         {
@@ -28,6 +31,7 @@ namespace Snake
             _graphics.PreferredBackBufferWidth = window_width;
             _graphics.PreferredBackBufferHeight = window_height;
             titlePos = new Vector2(window_width / 2 - 100, 10);
+            headPos = new Vector2(100, 100);
             base.Initialize();
         }
 
@@ -35,6 +39,7 @@ namespace Snake
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             font = Content.Load<SpriteFont>(fontName);
+            snakePart = Content.Load<Texture2D>(partName);
         }
 
         protected override void Update(GameTime gameTime)
@@ -51,6 +56,7 @@ namespace Snake
 
             _spriteBatch.Begin();
             _spriteBatch.DrawString(font, "Snake Game", titlePos, Color.Pink);
+            _spriteBatch.Draw(snakePart, headPos, Color.White);
             _spriteBatch.End();
             base.Draw(gameTime);
         }
--- Snake/obj/Debug/net6.0/Snake.AssemblyInfo.cs
+++ Snake/obj/Debug/net6.0/Snake.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a180718be42532acc9de0fc611f51a876c402b04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5f8a2bcf1303f3f25685d329084bddb1d918a5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
