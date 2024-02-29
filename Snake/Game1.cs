c5f8a2bcf1303f3f25685d329084bddb1d918a5b
TheGoldKay <jhonnatanwfc@live.com>
2024-02-29 19:14:24 -0300

    no borders (failure)

--- Snake/Game1.cs
+++ Snake/Game1.cs
@@ -9,8 +9,7 @@ namespace Snake
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        string fontName1 = "Arial";
-        string fontName2 = "gamefont";
+        string fontName = "gamefont";
         SpriteFont font;
         Vector2 titlePos;
         int window_width = 800;
@@ -25,7 +24,6 @@ namespace Snake
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
             this.Window.IsBorderless = true;
             _graphics.PreferredBackBufferWidth = window_width;
             _graphics.PreferredBackBufferHeight = window_height;
@@ -36,16 +34,13 @@ namespace Snake
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            font = Content.Load<SpriteFont>(fontName2);
-            // TODO: use this.Content to load your game content here
+            font = Content.Load<SpriteFont>(fontName);
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
@@ -54,9 +49,8 @@ namespace Snake
         {
             GraphicsDevice.Clear(Color.Crimson);
 
-            // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            _spriteBatch.DrawString(font, "Snake Game", titlePos, Color.White);
+            _spriteBatch.DrawString(font, "Snake Game", titlePos, Color.Pink);
             _spriteBatch.End();
             base.Draw(gameTime);
         }
